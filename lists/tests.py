6ad584d32fac3cc266201c0fefee36ffaa16aa5f
darinabird <darinabird46722@gmail.com>
2020-07-05 13:52:11 +0300

    Resolve error with list

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,4 @@
-from lists.models import Item
+from lists.models import Item, List
 from django.test import TestCase
 
 
@@ -36,8 +36,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
